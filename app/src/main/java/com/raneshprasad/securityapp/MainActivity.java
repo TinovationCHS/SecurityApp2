354898c10009df341f684d2ee271de73d9ccfdc2
raneshandroid <prasad_ranesh@yahoo.com>
2016-09-25 21:36:38 -0700

    Initial commit

---
+++ app/src/main/java/com/raneshprasad/securityapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.raneshprasad.securityapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button logIn;
+    Button signUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        logIn = (Button)findViewById(R.id.button);
+        signUp = (Button)findViewById(R.id.button2);
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), LogIn.class);
+                startActivity(i);
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(i);
+            }
+        });
+    }
+}
