a6b797f18d250cd045b32ded57b4eec1840b4cb2
raneshandroid <prasad_ranesh@yahoo.com>
2016-10-01 23:54:36 -0700

    I have made some changes in the id for various widgets.  This will help us recognize them easily especially when doing backend.

--- app/src/main/java/com/raneshprasad/securityapp/MainActivity.java
+++ app/src/main/java/com/raneshprasad/securityapp/MainActivity.java
@@ -13,8 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        logIn = (Button)findViewById(R.id.button);
-        signUp = (Button)findViewById(R.id.button2);
+        logIn = (Button)findViewById(R.id.button_logIn);
+        signUp = (Button)findViewById(R.id.button_signUp);
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
